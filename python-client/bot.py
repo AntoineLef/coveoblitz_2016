10e329007079e5c424bcdc143cd9e63a00eabd0a
Maxime Roussin-Belanger <maxime.roussinbelanger@gmail.com>
2016-01-09 09:05:37 -0500

    distanceZ

--- python-client/bot.py
+++ python-client/bot.py
@@ -1,9 +1,30 @@
 from random import choice
 import time
 from game import Game
+from game import Hero
 
 class Bot:
+    def move(self, state):
+        game = Game(state)
+        game.heroes_locs.keys
+        game.mines_locs
+        game.spikes_locs
+        game.taverns_locs
+        self.getNearestEnemy(game.myHero, game.mines_locs)
+
+
     pass
+
+    def getNearestMine(self, myHero, mines_locs):
+        mines_locs
+
+    def distance(self, pos1, pos2):
+        x = abs(pos2[0] - pos1[0])
+        y = abs(pos2[1] - pos1[1])
+        return (x,y)
+
+
+
 
 class RandomBot(Bot):
     def move(self, state):
